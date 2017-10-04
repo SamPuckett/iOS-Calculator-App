bb568410a91051ddea5330a170815cc1cd381163
Sam <samuelpuckett77@gmail.com>
2017-10-04 13:09:58 -0700

    Created functions for the signs, created function to see if last button pressed was mode, created function to update the label, created function to update mode. Linked the buttons to functions, created variables, and linked the label output to a variable in code. Made the Buttons change the output on screen. Made the clear button clear the display on screen. Made the + & - button look like they're working but aren't fully functional yet.

--- iOS Calculator App/ViewController.swift
+++ iOS Calculator App/ViewController.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
+enum modes {
+    case not_set
+    case addition
+    case subtraction
+}
+
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    var labelString:String = "0"
+    var currentModes:modes = modes.not_set
+    var numSaved:Int = 0
+    var lastButtonWasMode:Bool = false
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +32,65 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func didPressPlus(_ sender: Any) {
+        changeMode(newMode: modes.addition)
+    }
+    
+    @IBAction func didPressSubtract(_ sender: Any) {
+        changeMode(newMode: modes.subtraction)
+        
+    }
+    
+    @IBAction func didPressEquals(_ sender: Any) {
+    }
+    
+    @IBAction func didPressClear(_ sender: Any) {
+        labelString = "0"
+        currentModes = modes.not_set
+        numSaved = 0
+        lastButtonWasMode = false
+        label.text = "0"
+    }
+    
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        // Got the string value that was inside the button that was Pressed
+        let stringValue:String? = sender.titleLabel?.text
+        
+        if (lastButtonWasMode == true) {
+            lastButtonWasMode = false
+            labelString = "0"
+        }
+        
+        // Append the string value on to the labelString
+        labelString = labelString.appending(stringValue!)
+        // Called updateText() to update the text of the label to the new
+        //    string with the appended number
+        updateText()
+        
+    }
+    
+    func updateText() {
+        guard let labelInt:Int = Int(labelString) else {
+            return
+        }
+        if (currentModes == modes.not_set) {
+            numSaved = labelInt
+        }
+        
+        label.text = "\(labelInt)"
+    }
+    
+    func changeMode(newMode:modes) {
+        if (numSaved == 0) {
+            return
+        }
+        
+        currentModes = newMode
+        lastButtonWasMode = true
+    }
+    
+    
 
 }
 
